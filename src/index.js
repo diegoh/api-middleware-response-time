b2d66edf7782861800160dd40354019c04e1f02e
Diego Herrera <608492+diegoh@users.noreply.github.com>
2020-01-25 16:15:52 +0000

    Add missing tests

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-const example = () => {
-  return true;
-};
+const responseTime = () =>
+  async function responseTimeMiddleware(ctx, next) {
+    const start = new Date();
+    try {
+      await next();
+    } catch (err) {
+      const ms = new Date() - start;
+      ctx.set('X-Response-Time', `${ms}ms`);
+      throw err;
+    }
+    const ms = new Date() - start;
+    ctx.set('X-Response-Time', `${ms}ms`);
+  };
 
-module.exports = example;
+module.exports = responseTime;
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,11 +1,64 @@
 const assert = require('assert');
-const example = require('./index');
+const td = require('testdouble');
 
 describe('src/index', () => {
-  it('exports an init function', () => {
-    assert.strictEqual(typeof example, 'function');
+  let setupResponseTime;
+  let responseTime;
+  let ctx;
+
+  beforeEach(() => {
+    ctx = {
+      set: td.function()
+    };
+    setupResponseTime = require('./index');
+    responseTime = setupResponseTime();
   });
-  it('returns true', () => {
-    assert.strictEqual(example(), true);
+
+  afterEach(() => {
+    td.reset();
+  });
+
+  it('exports a function', () => {
+    assert.strictEqual(typeof responseTime, 'function');
+  });
+
+  it('calls next()', async () => {
+    const next = td.function();
+
+    await responseTime(ctx, next);
+    td.verify(next());
+  });
+
+  it('sets X-Response-Time header', async () => {
+    const captor = td.matchers.captor();
+
+    const next = td.function();
+
+    await responseTime(ctx, next);
+    td.verify(ctx.set('X-Response-Time', captor.capture()));
+
+    assert.ok(/^\d+ms$/.test(captor.values[0]));
+  });
+
+  describe('handling errors', () => {
+    it('sets X-Response-Time header', async () => {
+      const captor = td.matchers.captor();
+
+      const next = td.function();
+
+      const error = new Error('Err!!');
+      td.when(next()).thenReject(error);
+
+      try {
+        await responseTime(ctx, next);
+        assert.fail('should have errored');
+      } catch (err) {
+        assert.strictEqual(err.message, 'Err!!');
+      }
+
+      td.verify(ctx.set('X-Response-Time', captor.capture()));
+
+      assert.ok(/^\d+ms$/.test(captor.values[0]));
+    });
   });
 });
